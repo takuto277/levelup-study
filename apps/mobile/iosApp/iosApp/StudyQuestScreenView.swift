b1e60fdaf946961cf998fb6b3a1f0ec83a76ce5c
takuto277 <takuto.72.soccer@gmail.com>
2026-07-25 16:17:16 +0900

    fix(ios): move Equatable extension to file scope

--- apps/mobile/iosApp/iosApp/StudyQuestScreenView.swift
+++ apps/mobile/iosApp/iosApp/StudyQuestScreenView.swift
@@ -36,8 +36,6 @@ private struct PlayerSpriteView: View {
         case walking(phaseTick: Int64)
     }
 
-    extension SpriteMode: Equatable {}
-
     @State private var currentFrame: Int = 0
 
     private var prefix: String {
@@ -101,6 +99,8 @@ private struct PlayerSpriteView: View {
         }
     }
 }
+
+extension PlayerSpriteView.SpriteMode: Equatable {}
 
 private func playerPrepAssetName() -> String? {
     let frames = collectFrames(prefix: "sprite_player_prep")
